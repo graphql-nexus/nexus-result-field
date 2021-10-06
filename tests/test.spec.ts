8ccc152007cc78d0f213c17375d9727c3e84e224
Jason Kuhrt <jasonkuhrt@me.com>
2021-10-05 23:46:46 -0400

    tests

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,9 +1,3 @@
-import * as Lib from '../src/index'
-
 it('works', () => {
   expect(nameof(it)).toEqual('it')
-})
-
-it('imports using paths config works relative', () => {
-  expect(typeof Lib.todo).toEqual('function')
 })
